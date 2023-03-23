eb32dfd1d629775503c840eaad687da8c51918ac
harshgupta20 <hgreatgupta2001@gmail.com>
2023-03-23 20:08:53 +0530

    taskbar UI completed

--- src/components/TaskBar.jsx
+++ src/components/TaskBar.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import React from 'react';
+import "../styles/TaskBar.css";
+import { IoChevronDown } from "react-icons/io5";
+import { MdSearch } from "react-icons/md";
 
 const TaskBar = () => {
   return (
-    <div>TaskBar</div>
+    <>
+      <div id="task">
+        <div id="task-body">
+          <div id="task-submit">
+            <button id="task-btn1">All Submissions</button>
+            <button id="task-btn2">Favourite Submissions</button>
+          </div>
+          <div id="task-filter">
+            <label id="task-label" htmlFor="">
+              <MdSearch />
+              <input id="task-search" type="text" placeholder='search...' />
+            </label>
+            <div id="task-dropdown">
+              <select id="task-select" name="">
+                <option id="task-option" value="">Newest</option>
+                <option id="task-option" value="">Oldest</option>
+              </select>
+              <IoChevronDown />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import HeroComponent from '../components/HeroComponent';
+import TaskBar from '../components/TaskBar';
 
 const Home = () => {
   return (
     <>
         <HeroComponent/>
+        <TaskBar/>
     </>
   )
 }
